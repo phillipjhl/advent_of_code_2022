461e1883f339fd5c2bfe7b099ad1a803e6391d22
Phillip Langland <Phillip.Langland@chiprewards.com>
2023-01-03 17:22:06 -0600

    formatted build instructions into dictionary and some extra data cleanup, also added script header and README

---
+++ day19/README.md
@@ -0,0 +1,6 @@
+# Day 19 of 2022 Advent of Code - Not Enough Minerals
+
+[Original Instuctions]("https://adventofcode.com/2022/day/19")
+
+
+
--- day19/main.py
+++ day19/main.py
@@ -1,43 +1,93 @@
-RULE_MAPPINGS: list = ["ORE", "CLAY", "OBSIDIAN", "GEODE"]
+"""
+Author - Phillip Langland
 
+This script reads the local prompt.txt file, and calculates the "level of quality" of all the blueprints provided in that file which it will print out in the console.
+
+"""
+
+RULE_MAPPINGS: list = ["ORE", "CLAY", "OBSIDIAN", "GEODE"]
 
 def main():
     global RULE_MAPPINGS
     # read the blueprints line by line
     with open('prompt.txt', 'r') as prompt_file:
         for line in prompt_file:
-            get_blueprint_config(line)
+            calculate_bp_quality(line)
 
-# Return
-def get_blueprint_config(line: str):
+def calculate_bp_quality(line: str):
     line = line.strip()
-    print('BLUEPRINT: {0}'.format(line))
     blueprint_tuple = line.split(":")
     
+    print('{0}:'.format(blueprint_tuple[0]))
     blueprint_rules = blueprint_tuple[1].split(".")
     blueprint_rules.pop()
 
-    build_instructions = create_rules(blueprint_rules)
+    build_instructions = create_build_instructions(blueprint_rules)
 
-    print(build_instructions)
+    print('BUILD INSTRUCTIONS: {0}'.format(build_instructions))
+
+    # TO DO: calculate the max number of geodes a blueprint can build over 24 minutes. 
+    # Each robot can collect 1 of its resource type per minute. 
+    # It also takes one minute for the robot factory (also conveniently from your pack) to construct any type of robot, 
+    # although it consumes the necessary resources available when construction begins. 
 
 
-def create_rules(rules: list):
+def create_build_instructions(rules: list):
     cost_mapping_dict = {}
 
     # map rules
     for i in range(len(rules)):
         rule = rules[i]
-        clean_rule = rule.strip()
-        clean_rule = clean_rule.split("and")
-        # if there are multiple instructions, splite them
-        if len(clean_rule) > 0:
-            ore_cost = clean_rule[0].split("costs")[-1].strip()
-            clean_rule[0] = ore_cost 
+        instruction = rule.strip()
 
-        cost_mapping_dict[RULE_MAPPINGS[i]] = clean_rule
+        # split on multiple instructions
+        instruction = instruction.split("and")
+
+        # if there are multiple instructions, we need to separate them out 
+        if len(instruction) > 0:
+            ore_cost = instruction[0].split("costs")[-1]
+            instruction[0] = ore_cost
+            
+            # for each cost, strip unwanted whitespace
+            instruction = strip_whitespaces(instruction)
+            
+        # need to make instruction list a dictionary
+        material_dict = build_material_dict(instruction)
+
+        # add cleaned up intructions to result dict to make output all instructions
+        cost_mapping_dict[RULE_MAPPINGS[i]] = material_dict
 
     return cost_mapping_dict
+
+def build_material_dict(materials_list: list):
+    """
+    Takes a list of materials and takes the material name to use as a key, and the count to use as a value
+
+    Returns the result dictionary.
+
+    """
+    material_dict = {}
+    for i in range(len(materials_list)):
+        material = materials_list[i]
+        count_material_tuple = material.split(" ")
+        count = count_material_tuple[0]
+        material = count_material_tuple[1].upper()
+
+        material_dict[material] = count
+
+    return material_dict
+
+def strip_whitespaces(list: list):
+    """
+    Strip whitespaces for all elements in a list, and return the result
+    """
+    stripped_list = []
+
+    for i in range(len(list)):
+        item = list[i]
+        stripped_list.append(item.strip())
+
+    return stripped_list
 
 if __name__ == "__main__":
     main()
